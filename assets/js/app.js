994aee4845e97f5021c1b301c3c20b36b076939c
Ken Otuo <mrkoajr@gmail.com>
2021-06-23 17:13:12 -0400

    constructing the timer function

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,36 @@
-//Tracking variables in quiz 
-var currentQuestionIndex = 0;
-var time = questions.length * 15;
+// variables to track
+var curretnQuestionIndex
+//var time = questions.length * 20;
 var timerId;
 
-//initialize variable to reference DOM elements 
-var questElem = document.getElementById("questions");
-var timerEl = document.getElementById("time");
-var optionElem = document.getElementById("options");
-var startBtn = document.getElementById("start");
+//Reference the variables in the DOM 
+var questElem = document.getElementById("questions")
+var countdownElem = document.getElementById("timer")
+var optionElem = document.getElementById("options")
 
-//function that starts the quiz
-function startQuiz() {
-  
-    // start timer
-    timerId = setInterval(clockTick, 1000);
-  
-    // show starting time
-    timerEl.textContent = time;
-  
-    getQuestion();
-  }
-  
- 
+//Start timer 
+const startTime = 2;
+let time = startTime * 60;
+
+setInterval(updateCountdown, 1000 )
+
+function updateCountdown() {
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  time = time < 0 ? 0 : time; 
+
+  countdownElem.innerHTML = `${minutes}: ${seconds}`
+  time--;
+}
+
+
+
+
+
+
+
+
+
