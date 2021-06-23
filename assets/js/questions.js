494c9aeddb64a57e2174c25244386c85e4afba28
Ken Otuo <mrkoajr@gmail.com>
2021-06-23 02:18:48 -0400

    creating the quiz questions

---
+++ assets/js/questions.js
@@ -0,0 +1,29 @@
+// Quiz question objects list 
+var questions = [
+    {
+       quesTerm: "Which HTML element do we put JavaScript in?",
+       options: ["<script>", "<javascript>", "<js>", "<scripts.js>"],
+       answer: "<script>"
+},
+    {
+        quesTerm: "How do you create a function in JavaScript?",
+        options: ["function:example()", "function = examaple()", "function => example", "function;example()"],
+        answer: "function = example()"
+},
+    {
+        quesTerm: "Which event occurs when a user clicks on an HTML element?",
+        options: ["onmouseover", "onclick", "onchange", "onmouseclick"],
+        answer: "onclick"
+},
+    {
+        quesTerm: "How do you declare a JavaScript variable?",
+        options: ["v carName;", "variable carName;", "var carName;", "declareVar carName;"],
+        answer: "var carName;"
+},
+    {
+        quesTerm: "Which operator is used to assign a value to a variable?",
+        options: ["*", "x", "=", "-"],
+        answer: "="
+},
+  ];
+  
